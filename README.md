ed6495594a8e5904bcbda4ded05e7e32ee0017ba
Callie Nipper <callie.nipper@gmail.com>
2021-03-24 17:50:47 -0700

    Created a for loop and a switch statement to add hyperlinks to the toc section

--- README.md
+++ README.md
@@ -1,32 +1,39 @@
-git add .
 
-    #test
+    #ReadMe Generator
     MIT
 
-    ##Description
-    test
+    ## Description
+    This is a ReadMe Generator that will create a high quality readme file to assist users in creating their readme
 
-    ##Table of Contents
-    Description,Installation,Usage,License,Tests,Contributors,Questions
+    ## Table of Contents
 
-    ##Installation
+    
+ * [Description](#description)
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [License](#license)
+ * [Tests](#tests)
+ * [Contributors](#contributors)
+ * [Questions](#questions)
+
+    ## Installation
     To install the required dependencies, run the following command:
-    test
+    init
 
-    ##Usage
-    test
+    ## Usage
+    case 'Installation':
 
-    ##License
+    ## License
     The project is licensed under the MIT license.
 
-    ##Tests
+    ## Tests
     Please run the following command to test:
-    test
+                    break; 
 
-    ##Contributors
-    test
+    ## Contributors
+    rjkghkjdrg
 
-    ##Questions?
-    Please email test (link to email)[mailto:test] with any questions.
-    (link to GitHub user)[https://github.com/test]
+    ## Questions?
+    Please email callie.nipper@gmail.com (link to email)[mailto:callie.nipper@gmail.com] with any questions.
+    Visit me on GitHub at (link to GitHub user)[https://github.com/calliebn]
     
--- index.js
+++ index.js
@@ -4,68 +4,105 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'gituser'
-        },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'gituser'
+    },
 
-        {
-            type: 'input',
-            message: 'What is the title of the project?',
-            name: 'title'
-        },
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title'
+    },
 
-        {
-            type: 'checkbox',
-            message: 'What should be in the Table of Contents?',
-            name: 'toc',
-            choices: ['Description', 'Installation', 'Usage', 'License', 'Tests', 'Contributors', 'Questions']
-        },
+    {
+        type: 'checkbox',
+        message: 'What should be in the Table of Contents?',
+        name: 'toc',
+        choices: ['Description', 'Installation', 'Usage', 'License', 'Tests', 'Contributors', 'Questions']
+    },
 
-        {
-            type: 'input',
-            message: 'What is the project description?',
-            name: 'description'
-        },
+    {
+        type: 'input',
+        message: 'What is the project description?',
+        name: 'description'
+    },
 
-        {
-            type: 'input',
-            message: 'How should users install the application?',
-            name: 'installation'
-        },
+    {
+        type: 'input',
+        message: 'How should users install the application?',
+        name: 'installation'
+    },
 
-        {
-            type: 'input',
-            message: 'How is the application used?',
-            name: 'usage'
-        },
+    {
+        type: 'input',
+        message: 'How is the application used?',
+        name: 'usage'
+    },
 
-        {
-            type: 'list',
-            message: 'What license should the project use?',
-            name: 'license',
-            choices: ['MIT', 'BSD', 'GPL', 'Apache']
-        },
+    {
+        type: 'list',
+        message: 'What license should the project use?',
+        name: 'license',
+        choices: ['MIT', 'GPL', 'Apache', 'BSD', 'None']
+    },
 
-        {
-            type: 'input',
-            message: 'What are the testong procedures?',
-            name: 'tests'
-        },
+    {
+        type: 'input',
+        message: 'What are the testing procedures?',
+        name: 'tests'
+    },
 
-        {
-            type: 'input',
-            message: 'Who are the contributors? (Please list the contributors GitHub usernames separated by commas)',
-            name: 'contributors'
-        },
+    {
+        type: 'input',
+        message: 'Who are the contributors? (Please list the contributors GitHub usernames separated by commas)',
+        name: 'contributors'
+    },
 
-        {
-            type: 'input',
-            message: 'Enter your email address so Users can reach you with questions.',
-            name: 'email'
+    {
+        type: 'input',
+        message: 'Enter your email address so Users can reach you with questions.',
+        name: 'email'
+    }
+];
+
+function tocRender(options) {
+    let tocLinks = []
+    for (var i = 0; i < options.length; i++) {
+        switch (options[i]) {
+            case 'Description':
+                tocLinks.push(`\n * [Description](#description)`)
+                break;
+
+            case 'Installation':
+                tocLinks.push(`\n * [Installation](#installation)`)
+                break;
+
+                case 'Usage':
+                tocLinks.push(`\n * [Usage](#usage)`)
+                break;
+
+                case 'License':
+                tocLinks.push(`\n * [License](#license)`)
+                break;
+
+                case 'Tests':
+                tocLinks.push(`\n * [Tests](#tests)`)
+                break;
+
+                case 'Contributors':
+                tocLinks.push(`\n * [Contributors](#contributors)`)
+                break;
+
+                case 'Questions':
+                tocLinks.push(`\n * [Questions](#questions)`)
+                break;
+
         }
-    ];
+    }
+    return tocLinks.join('')
+}
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
@@ -73,45 +110,45 @@ function writeToFile(data) {
     #${data.title}
     ${data.license}
 
-    ##Description
+    ## Table of Contents
+    ${tocRender(data.toc)}
+
+    ## Description
     ${data.description}
 
-    ##Table of Contents
-    ${data.toc}
-
-    ##Installation
+    ## Installation
     To install the required dependencies, run the following command:
     ${data.installation}
 
-    ##Usage
+    ## Usage
     ${data.usage}
 
-    ##License
+    ## License
     The project is licensed under the ${data.license} license.
 
-    ##Tests
+    ## Tests
     Please run the following command to test:
     ${data.tests}
 
-    ##Contributors
+    ## Contributors
     ${data.contributors}
 
-    ##Questions?
-    Please email ${data.email} (link to email)[mailto:${data.email}] with any questions.
-    (link to GitHub user)[https://github.com/${data.gituser}]
+    ## Questions?
+    Please email ${data.email} with any questions.
+    Visit me on GitHub at https://github.com/${data.gituser}
     `
-return markText
+    return markText
 }
 
 // TODO: Create a function to initialize app
-function init() { 
+function init() {
     inquirer
-    .prompt(questions)
-    .then((response) => {
-        const fileName = writeToFile(response)
-        fs.writeFile('ReadMe.md', fileName, (err) =>
-            err ? console.error(err) : console.log('File created!'))
-    });
+        .prompt(questions)
+        .then((response) => {
+            const fileName = writeToFile(response)
+            fs.writeFile('ReadMe.md', fileName, (err) =>
+                err ? console.error(err) : console.log('File created!'))
+        });
 }
 
 // Function call to initialize app
